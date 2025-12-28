e3cc2d3d1751a869249f1af691d9a30241cac559
Ryuichi Eto <ryuichi.eto0315@gmail.com>
2025-12-29 04:08:03 +0900

    sln_update

--- obj/Debug/net10.0/win-x64/TaskManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/win-x64/TaskManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a832595827c529c4c4f1361703b568cbbc87f83")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
